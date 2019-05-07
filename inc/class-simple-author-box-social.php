fd6cdaeb9507b83edf320c056212587607bb6e48
razvanaldea89 <razvan.aldea89@yahoo.ro>
2019-05-07 20:57:23 +0300

    fix for added extra socials in pro version

--- inc/class-simple-author-box-social.php
+++ inc/class-simple-author-box-social.php
@@ -16,7 +16,10 @@ class Simple_Author_Box_Social {
 			$icon_svg = self::circle_long_shadow_icon( $icon );
 		}
 
-		$icon_svg = apply_filters( "sab_{$icon}_icon", $icon_svg );
+		if(has_filter("sab_{$icon}_icon")){
+			$icon_svg = apply_filters( "sab_{$icon}_icon", $icon );
+		}
+
 		$icon_svg = apply_filters( "sab_icon", $icon_svg );
 
 		return $icon_svg;
